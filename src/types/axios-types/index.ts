50f406d8da0fdebe81f2cf90229ec20bf60f7246
Mikita Havryliuk <chinter.games1@gmail.com>
2023-07-14 18:15:55 +0300

    some changes for axios-types

--- src/types/axios-types/index.ts
+++ src/types/axios-types/index.ts
@@ -1,5 +1,20 @@
-export type NearEarthObjects = object[]; 
+import { NeowInfo } from "../neows-info";
+
+interface NearEarthObjects {
+  "2023-07-01": NeowInfo;
+  "2023-07-02": NeowInfo;
+  "2023-07-03": NeowInfo;
+  "2023-07-04": NeowInfo;
+  "2023-07-05": NeowInfo;
+  "2023-07-06": NeowInfo;
+  "2023-07-07": NeowInfo;
+  "2023-07-08": NeowInfo;
+}
+
+interface ResponseData {
+  near_earth_objects: NearEarthObjects[];
+}
 
 export interface AxiosResponse {
-    data: NearEarthObjects | undefined;
+  data: ResponseData | undefined;
 }
