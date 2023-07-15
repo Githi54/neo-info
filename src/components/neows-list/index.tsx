95d19e6db5c74b1d45b450eecada883536921ebc
Mikita Havryliuk <chinter.games1@gmail.com>
2023-07-15 21:57:35 +0300

    remove comment

--- src/components/neows-list/index.tsx
+++ src/components/neows-list/index.tsx
@@ -42,53 +42,6 @@ export const NeowsList: React.FC<Props> = ({ neows }) => {
           <NeoItem key={date} date={date} index={index} />
         ))}
       </tbody>
-
-      {/* <tbody>
-          {todos.map((todo) => (
-            <tr
-              data-cy="todo"
-              className={classNames(
-                { 'has-background-info-light': selectedTodo?.id === todo.id },
-              )}
-              key={todo.id}
-            >
-              <td className="is-vcentered">{todo.id}</td>
-              <td className="is-vcentered">
-                {todo.completed && (
-                  <span className="icon" data-cy="iconCompleted">
-                    <i className="fas fa-check" />
-                  </span>
-                )}
-              </td>
-              <td className="is-vcentered is-expanded">
-                <p className={classNames(
-                  { 'has-text-success': todo.completed },
-                  { 'has-text-danger': !todo.completed },
-                )}
-                >
-                  {todo.title}
-                </p>
-              </td>
-              <td className="has-text-right is-vcentered">
-                <button
-                  data-cy="selectButton"
-                  className="button"
-                  type="button"
-                  onClick={() => callbackTodo(todo)}
-                >
-                  <span className="icon">
-                    <i className={classNames(
-                      'far',
-                      { 'fa-eye': todo !== selectedTodo },
-                      { 'fa-eye-slash': todo === selectedTodo },
-                    )}
-                    />
-                  </span>
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody> */}
     </table>
   );
 };
